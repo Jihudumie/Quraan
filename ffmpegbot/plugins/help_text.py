899e488049c6855bd8b13b6e6a4930d7435e4414
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-06-01 07:12:07 +0300

    Update help_text.py

--- ffmpegbot/plugins/help_text.py
+++ ffmpegbot/plugins/help_text.py
@@ -10,63 +10,63 @@ async def start_text(client, message):
 async def msaada(client, message):
     await message.reply_text(MSAADA_TXT, quote=True)
 
-@Client.on_message(Filters.command(["1na2"]))
+@Client.on_message(Filters.command(["1_2"]))
 async def moja(client, message):
     await message.reply_text(MOJA, quote=True)
 
-@Client.on_message(Filters.command(["3na4"]))
+@Client.on_message(Filters.command(["3_4"]))
 async def mbili(client, message):
     await message.reply_text(MBILI, quote=True)
 
-@Client.on_message(Filters.command(["5na6"]))
+@Client.on_message(Filters.command(["5_6"]))
 async def tatu(client, message):
     await message.reply_text(TATU, quote=True)
 
-@Client.on_message(Filters.command(["7na8"]))
+@Client.on_message(Filters.command(["7_8"]))
 async def nne(client, message):
     await message.reply_text(NNE, quote=True)
 
-@Client.on_message(Filters.command(["9na10"]))
+@Client.on_message(Filters.command(["9_10"]))
 async def tano(client, message):
     await message.reply_text(TANO, quote=True)
 
-@Client.on_message(Filters.command(["11na12"]))
+@Client.on_message(Filters.command(["11_12"]))
 async def sita(client, message):
     await message.reply_text(SITA, quote=True)
 
-@Client.on_message(Filters.command(["13na14"]))
+@Client.on_message(Filters.command(["13_14"]))
 async def saba(client, message):
     await message.reply_text(SABA, quote=True)
 
-@Client.on_message(Filters.command(["15na16"]))
+@Client.on_message(Filters.command(["15_16"]))
 async def nane(client, message):
     await message.reply_text(NANE, quote=True)
 
-@Client.on_message(Filters.command(["17na18"]))
+@Client.on_message(Filters.command(["17_18"]))
 async def tisa(client, message):
     await message.reply_text(TISA, quote=True)
 
-@Client.on_message(Filters.command(["19na20"]))
+@Client.on_message(Filters.command(["19_20"]))
 async def kumi(client, message):
     await message.reply_text(KUMI, quote=True)
 
-@Client.on_message(Filters.command(["21na22"]))
+@Client.on_message(Filters.command(["21_22"]))
 async def kumi_namoja(client, message):
     await message.reply_text(KMOJA, quote=True)
 
-@Client.on_message(Filters.command(["23na24"]))
+@Client.on_message(Filters.command(["23_24"]))
 async def kumi_nwmbili(client, message):
     await message.reply_text(KMBILI, quote=True)
 
-@Client.on_message(Filters.command(["25na26"]))
+@Client.on_message(Filters.command(["25_26"]))
 async def kumi_natatu(client, message):
     await message.reply_text(KTATU, quote=True)
 
-@Client.on_message(Filters.command(["27na28"]))
+@Client.on_message(Filters.command(["27_28"]))
 async def Kumi_nne(client, message):
     await message.reply_text(KNNE, quote=True)
 
-@Client.on_message(Filters.command(["29na30"]))
+@Client.on_message(Filters.command(["29_30"]))
 async def kumi_tano(client, message):
     await message.reply_text(KTANO, quote=True)
 
